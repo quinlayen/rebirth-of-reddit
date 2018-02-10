138d8c0233f6acaeb766673033db4544060f00a6
quinlayen <quinlayen@gmail.com>
2018-02-10 12:54:56 -1000

    added images to contentBoxes

--- js/app.js
+++ js/app.js
@@ -1,29 +1,44 @@
 console.log('sanity check');
 
 const request = (url, callback) => {
-    const oReq = new XMLHttpRequest();
-    oReq.addEventListener('load', callback)
-    oReq.open('GET', url);
-    oReq.send();
-}
+  const oReq = new XMLHttpRequest();
+  oReq.addEventListener('load', callback);
+  oReq.open('GET', url);
+  oReq.send();
+};
 
-url = 'https://www.reddit.com/r/boardgames.json'
+url = 'https://www.reddit.com/r/boardgames.json';
 
+/*
+Dynamically create and populate posts into a group of contentBox.
+Each content box should include a thumbnail (if available), title, author, and caption or 
+shortened version of posts. When the box is clicked on it should expand showing the entire post.
+*/
 request(url, function() {
-    const data = JSON.parse(this.responseText);
-    const contentContainer = document.getElementById('content_container');
-    data.data.children.forEach(element =>{
-        const title = element.data.title;
-        const selfTextData = element.data.selftext;
-        const author = element.data.author;
-        const permalink = element.data.permalink;
-        let contentBox = document.createElement('div');
-        contentBox.className='content_box';
-        contentContainer.appendChild(contentBox);
-        let t = document.createTextNode(selfTextData);
-        contentBox.appendChild(t);   
-    })
-   
+  const data = JSON.parse(this.responseText);
+  const contentContainer = document.getElementById('content_container');
+  data.data.children.forEach(element => {
+    //variables for data collection
+    const title = element.data.title;
+    const selfTextData = element.data.selftext;
+    const author = element.data.author;
+    const defaultImage = '../assets/Placeholder.jpg'
+    const image = (element.data.preview) ? element.data.preview.images[0].source.url : defaultImage;
+   console.log(image)
+    const permalink = element.data.permalink;
+    //create dom elements to connect variables to
+    //create contentBox which is the parent element for posts
+    const contentBox = document.createElement('div');
+    contentBox.className = 'content_box';
+    contentContainer.appendChild(contentBox);
 
+    //create image element and put images into contentBox
+    const imageBox = document.createElement('IMG');
+    imageBox.className = 'image_box';
+    imageBox.src = image;
+    contentBox.appendChild(imageBox);
 
+    // let t = document.createTextNode(selfTextData);
+    // contentBox.appendChild(t);
+  });
 });
