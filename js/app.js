26464fc5909743273c2f93d58890767ed01fcea0
quinlayen <quinlayen@gmail.com>
2018-02-13 22:45:34 -1000

    added functionality for all 3 event listeners.  Now need to 1) figure out what happened to pictures and 2) how to erase previous data when showing new data

--- js/app.js
+++ js/app.js
@@ -14,96 +14,103 @@ Dynamically create and populate posts into a group of contentBox.
 Each content box should include a thumbnail (if available), title, author, and caption or 
 shortened version of posts. When the box is clicked on it should expand showing the entire post.
 */
-const mainRequest = function(url) {
-  request();
+
+const requestListener = url => {
+  request(url, function() {
+    const data = JSON.parse(this.responseText);
+    const contentContainer = document.getElementById('content_container');
+    data.data.children.forEach(element => {
+      //variables for data collection
+      const title = element.data.title;
+      const selfTextData = element.data.selftext;
+      const author = element.data.author;
+      const submittedUtc = element.data.created_utc;
+      const defaultImage = '../../assets/Placeholder.jpg';
+      const image = element.data.preview
+        ? element.data.preview.images[0].source.url
+        : defaultImage;
+      //create dom elements to connect variables to
+      //create contentBox which is the parent element for posts
+      const contentBox = document.createElement('div');
+      contentBox.className = 'content_box';
+      contentContainer.appendChild(contentBox);
+
+      //create image element and put images into contentBox
+      const imageBox = document.createElement('IMG');
+      imageBox.className = 'image_box';
+      imageBox.src = image;
+      contentBox.appendChild(imageBox);
+
+      //create title content
+      const titleText = document.createElement('div');
+      titleText.className = 'title_text';
+      contentBox.appendChild(titleText);
+      titleText.innerHTML = title;
+
+      //this function calculates the time when the post was submitted and displays it along with the author
+      const calculatetime = submittedUtc => {
+        const submittedDate = new Date(submittedUtc * 1000);
+        const todaysDate = new Date();
+        const howLongAgo = todaysDate - submittedDate;
+        //convert difference from posted and today's date into a readable time
+        let seconds = Math.floor(howLongAgo / 1000);
+        let minutes = Math.floor(seconds / 60);
+        let hours = Math.floor(minutes / 60);
+        let days = Math.floor(hours / 24);
+
+        //convert days into years with days left over
+        while (days > 365) {
+          let years = Math.floor(days / 365);
+          days = days % 365;
+          return `Submitted ${years} years and ${days} days ago by ${author}`;
+        }
+        //convert hours into days with hours left over
+        if (hours > 24) {
+          days = Math.floor(hours / 24);
+          hours = hours % 24;
+          return `Submitted ${days} days and ${hours} hours ago by ${author}`;
+        }
+        //convert minutes into hours with minutes left over
+        while (minutes > 60) {
+          hours = Math.floor(minutes / 60);
+          minutes = minutes % 60;
+          return `Submitted ${hours} hours and ${minutes} minutes ago by ${author}`;
+        }
+        //convert seconds into minutes with seconds left over
+        while (seconds > 60) {
+          minutes = Math.floor(seconds / 60);
+          seconds = seconds % 60;
+          return `Submitted ${minutes} hours and ${seconds} minutes ago by ${author}`;
+        }
+      };
+
+      //create author content
+      const authorTimeText = document.createElement('div');
+      authorTimeText.className = 'author_text';
+      contentBox.appendChild(authorTimeText);
+      authorTimeText.innerHTML = calculatetime(submittedUtc);
+
+      //create text content of each post
+      const postText = document.createElement('p');
+      postText.className = 'post_text';
+      contentBox.appendChild(postText);
+      postText.innerHTML = selfTextData;
+    }); //end of forEach loop
+  });
 };
-request(url, function() {
-  const data = JSON.parse(this.responseText);
-  const contentContainer = document.getElementById('content_container');
-  data.data.children.forEach(element => {
-    //variables for data collection
-    const title = element.data.title;
-    const selfTextData = element.data.selftext;
-    const author = element.data.author;
-    const submittedUtc = element.data.created_utc;
-    const defaultImage = '../assets/Placeholder.jpg';
-    const image = element.data.preview
-      ? element.data.preview.images[0].source.url
-      : defaultImage;
-    const permalink = element.data.permalink;
-    //create dom elements to connect variables to
-    //create contentBox which is the parent element for posts
-    const contentBox = document.createElement('div');
-    contentBox.className = 'content_box';
-    contentContainer.appendChild(contentBox);
+requestListener(url);
 
-    //create image element and put images into contentBox
-    const imageBox = document.createElement('IMG');
-    imageBox.className = 'image_box';
-    imageBox.src = image;
-    contentBox.appendChild(imageBox);
-
-    //create title content
-    const titleText = document.createElement('div');
-    titleText.className = 'title_text';
-    contentBox.appendChild(titleText);
-    titleText.innerHTML = title;
-
-    //this function calculates the time when the post was submitted and displays it along with the author
-    const calculatetime = submittedUtc => {
-      const submittedDate = new Date(submittedUtc * 1000);
-      const todaysDate = new Date();
-      const howLongAgo = todaysDate - submittedDate;
-      //convert difference from posted and today's date into a readable time
-      let seconds = Math.floor(howLongAgo / 1000);
-      let minutes = Math.floor(seconds / 60);
-      let hours = Math.floor(minutes / 60);
-      let days = Math.floor(hours / 24);
-
-      //convert days into years with days left over
-      while (days > 365) {
-        let years = Math.floor(days / 365);
-        days = days % 365;
-        return `Submitted ${years} years and ${days} days ago by ${author}`;
-      }
-      //convert hours into days with hours left over
-      if (hours > 24) {
-        days = Math.floor(hours / 24);
-        hours = hours % 24;
-        return `Submitted ${days} days and ${hours} hours ago by ${author}`;
-      }
-      //convert minutes into hours with minutes left over
-      while (minutes > 60) {
-        hours = Math.floor(minutes / 60);
-        minutes = minutes % 60;
-        return `Submitted ${hours} hours and ${minutes} minutes ago by ${author}`;
-      }
-      //convert seconds into minutes with seconds left over
-      while (seconds > 60) {
-        minutes = Math.floor(seconds / 60);
-        seconds = seconds % 60;
-        return `Submitted ${minutes} hours and ${seconds} minutes ago by ${author}`;
-      }
-    };
-
-    //create author content
-    const authorTimeText = document.createElement('div');
-    authorTimeText.className = 'author_text';
-    contentBox.appendChild(authorTimeText);
-    authorTimeText.innerHTML = calculatetime(submittedUtc);
-
-    //create text content of each post
-    const postText = document.createElement('p');
-    postText.className = 'post_text';
-    contentBox.appendChild(postText);
-    postText.innerHTML = selfTextData;
-  }); //end of forEach loop
+const getApp = document.getElementById('get_app');
+getApp.addEventListener('click', function() {
+  requestListener('https://www.reddit.com/r/reactjs.json');
 });
 
-window.onload = mainRequest(url);
-
 const myBoards = document.getElementById('my_boards');
-myBoards.addEventListener(
-  'click',
-  request('https://www.reddit.com/r/reactjs.json')
-);
+myBoards.addEventListener('click', function() {
+  requestListener('https://www.reddit.com/r/boardgames.json');
+});
+
+const random = document.getElementById('get_app');
+random.addEventListener('click', function() {
+  requestListener('https://www.reddit.com/r/random.json');
+});
